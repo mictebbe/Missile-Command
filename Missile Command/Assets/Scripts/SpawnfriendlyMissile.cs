e3f44511230a05eb8755c7ece8b1cbeb3b65cf76
Michael Tebbe <michaeltebbe1991@gmail.com>
2016-10-31 12:39:35 +0100

    1. a) Spawn friendly missiles randomly

---
+++ Missile Command/Assets/Scripts/SpawnfriendlyMissile.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnfriendlyMissile : MonoBehaviour {
+    public GameObject missilePrefab;
+    public int amount;
+
+    private ArrayList missiles = new ArrayList();    private int currentAmount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < amount; i++)
+        {            GameObject temp = Instantiate(missilePrefab) as GameObject;            temp.transform.localScale = new Vector3(1, 1, 1);            temp.transform.parent = transform;            temp.SetActive(false);            //temp.GetComponent<Renderer>().material.color = Random.ColorHSV();
+
+            missiles.Add(temp);
+
+            if (temp.GetComponent<friendlyMissileScript>() == null)
+            {
+                temp.AddComponent<friendlyMissileScript>();            }
+
+            currentAmount++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+            if (Time.frameCount % 30 == 0 && currentAmount > 0)
+            {
+                GameObject current = (GameObject)missiles[currentAmount-1];
+
+                current.SetActive(true);
+
+                currentAmount--;
+
+            }
+        }
+}
---
+++ Missile Command/Assets/Scripts/friendlyMissileScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class friendlyMissileScript : MonoBehaviour {
+    public float speed=1;
+    private GameObject parent;
+    private Vector3 direction;
+
+    // Use this for initialization
+    void Start () {
+        parent = gameObject.transform.parent.gameObject;
+        direction = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f))*speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (parent)
+        {
+            //gameObject.transform.RotateAround(parent.transform.position, up, speed * Time.deltaTime);
+        }
+        
+        gameObject.transform.Translate(direction);
+    }
+}
