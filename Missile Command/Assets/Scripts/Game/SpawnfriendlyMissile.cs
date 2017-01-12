cdcd1790e4ebcaf1f2cfa2d0c619e1fa82ae9dfa
Tarek S <tarek.schneider@beuth-hochschule.de>
2017-01-12 11:56:42 +0100

    Missile Path

--- Missile Command/Assets/Scripts/Game/SpawnfriendlyMissile.cs
+++ Missile Command/Assets/Scripts/Game/SpawnfriendlyMissile.cs
@@ -1,56 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SpawnfriendlyMissile : MonoBehaviour {
-    public GameObject missilePrefab;
-    public int amount;
-    public float speed=1;
-    public GameObject cursor;
-    public GameObject friendlyMissileExplosion;
-    public UnityEngine.KeyCode buttonNumber = KeyCode.A;
-    private ArrayList missiles = new ArrayList();    private int currentAmount = 0;
-    
-    // Use this for initialization
-    void Start()
-    {
-        for (int i = 0; i < amount; i++)
-        {            GameObject temp = Instantiate(missilePrefab) as GameObject;
-            //temp.transform.localScale = new Vector3(1, 1, 1);
-            //temp.transform.parent = transform;
-            temp.transform.position = new Vector3(0, 0, 0);//gameObject.transform.position;            temp.SetActive(false);            //temp.GetComponent<Renderer>().material.color = Random.ColorHSV();
+public class SpawnfriendlyMissile : MonoBehaviour
+{
+	public GameObject missilePrefab;
+	public int amount;
+	public float speed = 1;
+	public GameObject cursor;
+	public GameObject friendlyMissileExplosion;
+	public UnityEngine.KeyCode buttonNumber = KeyCode.A;
+	private ArrayList missiles = new ArrayList();
+	private int currentAmount = 0;
 
-            missiles.Add(temp);
+	// Use this for initialization
+	void Start()
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			GameObject temp = Instantiate(missilePrefab) as GameObject;
+			//temp.transform.localScale = new Vector3(1, 1, 1);
+			//temp.transform.parent = transform;
+			//temp.transform.position = new Vector3(0, 0, 0);//gameObject.transform.position;
 
-            if (temp.GetComponent<friendlyMissileScript>() == null)
-            {
-                temp.AddComponent<friendlyMissileScript>();            }
-            
-            
+			//temp.GetComponent<Renderer>().material.color = Random.ColorHSV();
 
-            currentAmount++;
-        }
-    }
-	
+			temp.SetActive(false);
+			missiles.Add(temp);
+
+			if (temp.GetComponent<friendlyMissileScript>() == null)
+			{
+				temp.AddComponent<friendlyMissileScript>();
+			}
+
+
+
+			currentAmount++;
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+		if (Input.GetKeyDown(buttonNumber))
+		{
+			if (currentAmount > 0)
+			{
+				GameObject current = (GameObject) missiles[currentAmount - 1];
+				current.GetComponent<friendlyMissileScript>().cursor = cursor;
+				current.GetComponent<friendlyMissileScript>().speed = speed;
+				current.GetComponent<friendlyMissileScript>().friendlyExplosionPrefab = friendlyMissileExplosion;
+				current.transform.Translate(gameObject.transform.position);
 
-        
-            if (Input.GetKeyDown(buttonNumber))
-            {
-            if (currentAmount > 0)
-            {
-                GameObject current = (GameObject)missiles[currentAmount - 1];
-                current.GetComponent<friendlyMissileScript>().cursor = cursor;
-                current.GetComponent<friendlyMissileScript>().speed = speed;
-                current.GetComponent<friendlyMissileScript>().friendlyExplosionPrefab = friendlyMissileExplosion;
-                current.transform.Translate(gameObject.transform.position);
-                current.SetActive(true);
+				current.SetActive(true);
 
-                currentAmount--;
-            }else
-            {
-                Debug.Log("No missiles left in Missile Launcher '" + gameObject.name+"'");
-            }
-            }
-        }
+				currentAmount--;
+			}
+			else
+			{
+				Debug.Log("No missiles left in Missile Launcher '" + gameObject.name + "'");
+			}
+		}
+	}
 }
--- Missile Command/Assets/Scripts/Game/friendlyMissileScript.cs
+++ Missile Command/Assets/Scripts/Game/friendlyMissileScript.cs
@@ -1,55 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class friendlyMissileScript : MonoBehaviour {
-    public float speed=1;
-    public GameObject cursor;
-    public GameObject friendlyExplosionPrefab;
-    private GameObject parent;
-    private Vector3 direction;
-    private Vector3 explosionPosition;
-    private GameObject friendlyExplosion;
-    // Use this for initialization
-    void Start () {
+public class friendlyMissileScript : MonoBehaviour
+{
+	public float speed = 1;
+	public GameObject cursor;
+	public GameObject friendlyExplosionPrefab;
+	private Vector3 direction;
+	private GameObject parent;
+	private Vector3 explosionPosition;
+	private GameObject friendlyExplosion;
+	// Use this for initialization
+	void Start()
+	{
 
-        explosionPosition = cursor.transform.position;
-        direction = Vector3.Normalize(explosionPosition- gameObject.transform.position);
-
-        Debug.Log("Explosion position: "+explosionPosition);
-        Debug.Log("Direction: " + direction);
-
-        friendlyExplosion = Instantiate(friendlyExplosionPrefab) as GameObject;
-        //friendlyExplosion.transform.localScale = new Vector3(1, 1, 1);
-        friendlyExplosion.transform.position = explosionPosition;
-        friendlyExplosion.SetActive(false);
-
-        if (friendlyExplosion.GetComponent<ExplosionScript>() == null)
-        {
-            friendlyExplosion.AddComponent<ExplosionScript>();
-        }
-
-    }
+		explosionPosition = cursor.transform.position;
 	
+
+		Debug.Log("Explosion position: " + explosionPosition);
+		Debug.Log("Direction: " + direction);
+
+		friendlyExplosion = Instantiate(friendlyExplosionPrefab) as GameObject;
+		//friendlyExplosion.transform.localScale = new Vector3(1, 1, 1);
+		friendlyExplosion.transform.position = explosionPosition;
+		friendlyExplosion.SetActive(false);
+
+		if (friendlyExplosion.GetComponent<ExplosionScript>() == null)
+		{
+			friendlyExplosion.AddComponent<ExplosionScript>();
+		}
+
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 
-        StartCoroutine(Fly());
-       
-        if (Vector3.Magnitude(gameObject.transform.position-explosionPosition)<=2*speed)
-        {
-           
-            Debug.Log("Friendly Missile explodes.");
-            friendlyExplosion.SetActive(true);
-            Destroy(gameObject);
+		StartCoroutine(Fly());
 
-        }
-    }
+		if (Vector3.Magnitude(gameObject.transform.position - explosionPosition) <= 2 * speed)
+		{
+
+			Debug.Log("Friendly Missile explodes.");
+			friendlyExplosion.SetActive(true);
+			Destroy(gameObject);
+
+		}
+	}
 
 
-    IEnumerator Fly()
-    {
-        gameObject.transform.Translate(direction * speed);
+	IEnumerator Fly()
+	{
+		var amplitude = 4.0f;
+		var speedMod = 0.7f; // redundant
+		var scale = 0.006f;
 
-        yield return null;
-    }
+		var missilePosition = gameObject.transform.position;
+		var path = explosionPosition - missilePosition;
+		direction = Vector3.Normalize(path);
+
+		var translation = direction * speed * speedMod;
+		var noise = amplitude *  (Mathf.PerlinNoise(path.x * scale, path.y * scale) - 0.5f);
+
+		var side = new Vector3(0, 0, 1);
+		var modDirection = Vector3.Cross(direction, side);
+		translation += modDirection * noise;
+
+		gameObject.transform.GetChild(0).transform.rotation = Quaternion.LookRotation(-translation);
+		gameObject.transform.Translate(translation);
+
+		yield return null;
+	}
 }
