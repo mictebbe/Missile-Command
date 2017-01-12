6135e0e0df23b601277aa58f321490a67b762ae7
Tarek S <tarek.schneider@beuth-hochschule.de>
2017-01-12 17:21:54 +0100

    helicopter

---
+++ Missile Command/Assets/Helicopter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+
+	private Vector3 startPos;
+	private float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += transform.forward * (-2.0f);
+
+		transform.Find("main_rotor").transform.Rotate(new Vector3(0, 0, 15));
+		
+		if(transform.position.x > 400.0)
+		{
+			transform.position = startPos;
+			this.enabled = false;
+		}
+		
+	}
+}
